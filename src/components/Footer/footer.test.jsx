53ee979fc148f4053a3d9ef0a58caeda11b77a1d
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-09-10 22:47:18 +0200

    Dev: Fixing all tests infrastructure

--- src/components/Footer/footer.test.jsx
+++ src/components/Footer/footer.test.jsx
@@ -1,5 +1,9 @@
-const { describe } = require("vitest");
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Footer from './index'
 
 describe('Should Footer component work perfectly', () => {
-  
+  it('Should the component be wrapped by a footer tag', () => {
+    render(<Footer />)
+  })
 })
