53cf77e98b2d94124109b95daa3cb608c960a98b
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-09-11 23:01:24 +0200

    Dev: Setting logic on basic task creation

--- src/components/create/index.tsx
+++ src/components/create/index.tsx
@@ -1,8 +1,35 @@
+import { useState, type FormEvent } from "react";
+import type { Task } from "../../models/Task.model";
+
 const Create = () => {
+  const [content, setContent] = useState<string>('');
+
+  const createTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(content.length < 3) {
+      return;
+    }
+
+    const newTask: Task = {
+      id: 1,
+      content: content.trim(),
+      created: new Date().toLocaleDateString(),
+    }
+    console.log('Création de nouvelle tâche : ', newTask);
+    setContent('');
+  } 
+
   return (
-    <form className="taskForm">
-      <input type="text" name="task" />
-      <button type="submit">Créer une tâche</button>
+    <form onSubmit={(e) => createTask(e)}>
+      <input 
+        type="text" 
+        id="task" 
+        value={content}
+        placeholder="Entrer une nouvelle tâche"
+        onChange={(event) => setContent(event.target.value)} 
+      />
+      <button type="submit" disabled={content.length < 3}>Créer une tâche</button>
     </form>
   );
 }
