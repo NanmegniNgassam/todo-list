caffdc5dcfd6306b290b0af944ec08670b8518be
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-09-18 21:23:20 +0200

    Dev: Ajusting mobile GUI

--- src/components/tasksWidget/index.tsx
+++ src/components/tasksWidget/index.tsx
@@ -2,16 +2,18 @@ import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 import PublishedWithChangesIcon from '@mui/icons-material/PublishedWithChanges';
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, Tab } from "@mui/material";
+import { Box, Tab, useMediaQuery, useTheme } from "@mui/material";
 import { useState } from "react";
 import { connect } from "react-redux";
 import TasksList from '../tasksList';
 import type { DataBase } from '../../store/reducers/taskReducer';
 
 const TasksWidget = (props: DataBase) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   type TabType = 'all' | 'done' | 'undone';
   const [value, setValue] = useState<TabType>('all');
-  const { tasks } = props 
+  const { tasks } = props;
 
   const handleChange = (_: React.SyntheticEvent, newValue: TabType) => {
     setValue(newValue);
@@ -24,19 +26,19 @@ const TasksWidget = (props: DataBase) => {
           <TabList onChange={handleChange} aria-label="Select a target on tasks you want to see.">
             <Tab 
               icon={<HourglassEmptyIcon color="primary" fontSize="small" />} 
-              iconPosition="end" 
+              iconPosition={isMobile ? 'top' : 'end'}  
               label="Toutes" 
               value="all" 
             />
             <Tab 
               icon={<DoneOutlineIcon color="success" fontSize="small" />} 
-              iconPosition="end" 
+              iconPosition={isMobile ? 'top' : 'end'} 
               label="Terminées" 
               value="done" 
             />
             <Tab 
               icon={<PublishedWithChangesIcon color="warning" fontSize="small" />} 
-              iconPosition="end" 
+              iconPosition={isMobile ? 'top' : 'end'} 
               label="Non terminées" 
               value="undone" 
             />
